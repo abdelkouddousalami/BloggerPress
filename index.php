905dff7cab68bb01149d50ac7804fbc8ec1372f9
abdelkouddous <abdoalami.ru@gmail.com>
2024-12-15 14:02:25 +0100

    Creer un diagramme des cas d’utilisation

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BloggerPress</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <img src="images/bloglogo-removebg-preview_1.jpg" alt="">
+        </div>
+        <ul class="nav-links">
+            <a href="index.php"><li>Home</li></a>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <a href="sign.php"><div class="login">
+            Author Area
+        </div></a>
+    </nav>
+    <main>
+
+        <section class="general">
+            <div class="content">
+                <h1>BloggerPress</h1>
+                <p>Your gateway to sharing stories, ideas, and creativity. Start blogging today with our simple, intuitive platform that puts you in control of your voice.</p>
+                <a href="sign.php" class="btn">Start Writing</a>
+            </div>
+            <div class="image">
+                <img src="images/general.jpeg" alt="Blogging Illustration">
+            </div>
+        </section>
+
+        <section class="container1">
+            <div class="blogs">
+                <img src="images/travel-blogger-800x450.jpg" alt="">
+                <h2>The world in 2100</h2>
+                <p class="textb">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate 
+                    esse officiis ad eligendi consequatur? Eaque ipsam recusandae 
+                    nam consectetur! Vel natus amet quisquam doloribus facere ...
+                </p>
+                <button class="btnblog">View More</button>
+                <h3>Writed By :</h3>
+                <p class="author">James Fr</p>
+            </div>
+            <div class="blogs">
+                <img src="images/travel-blogger-800x450.jpg" alt="">
+                <h2>The world in 2100</h2>
+                <p class="textb">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate 
+                    esse officiis ad eligendi consequatur? Eaque ipsam recusandae 
+                    nam consectetur! Vel natus amet quisquam doloribus facere ...
+                </p>
+                <button class="btnblog">View More</button>
+                <h3>Writed By :</h3>
+                <p class="author">James Fr</p>
+            </div>
+            <div class="blogs">
+                <img src="images/travel-blogger-800x450.jpg" alt="">
+                <h2>The world in 2100</h2>
+                <p class="textb">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate 
+                    esse officiis ad eligendi consequatur? Eaque ipsam recusandae 
+                    nam consectetur! Vel natus amet quisquam doloribus facere ...
+                </p>
+                <button class="btnblog">View More</button>
+                <h3>Writed By :</h3>
+                <p class="author">James Fr</p>
+            </div>
+        </section>
+        <section class="container2">
+            <div class="blogs">
+                <img src="images/travel-blogger-800x450.jpg" alt="">
+                <h2>The world in 2100</h2>
+                <p class="textb">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate 
+                    esse officiis ad eligendi consequatur? Eaque ipsam recusandae 
+                    nam consectetur! Vel natus amet quisquam doloribus facere ...
+                </p>
+                <button class="btnblog">View More</button>
+                <h3>Writed By :</h3>
+                <p class="author">James Fr</p>
+            </div>
+            <div class="blogs">
+                <img src="images/travel-blogger-800x450.jpg" alt="">
+                <h2>The world in 2100</h2>
+                <p class="textb">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate 
+                    esse officiis ad eligendi consequatur? Eaque ipsam recusandae 
+                    nam consectetur! Vel natus amet quisquam doloribus facere ...
+                </p>
+                <button class="btnblog">View More</button>
+                <h3>Writed By :</h3>
+                <p class="author">James Fr</p>
+            </div>
+            <div class="blogs">
+                <img src="images/travel-blogger-800x450.jpg" alt="">
+                <h2>The world in 2100</h2>
+                <p class="textb">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate 
+                    esse officiis ad eligendi consequatur? Eaque ipsam recusandae 
+                    nam consectetur! Vel natus amet quisquam doloribus facere ...
+                </p>
+                <button class="btnblog">View More</button>
+                <h3>Writed By :</h3>
+                <p class="author">James Fr</p>
+            </div>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
